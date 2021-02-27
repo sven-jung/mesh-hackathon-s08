e946bff667020f45824f88e79ffab0ab6965f47d
Sven Jung <github@svenjung.me>
2021-02-27 11:51:32 +0100

    Create in_chat.dart

---
+++ mesh_app_social/lib/chat/in_chat.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class InChatView extends StatefulWidget {
+  @override
+  _InChatViewState createState() => _InChatViewState();
+}
+
+class _InChatViewState extends State<InChatView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

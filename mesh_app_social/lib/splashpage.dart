332175718f96894b8230f03d1bc4af2e2f24aa47
who-stel <61997708+who-stel@users.noreply.github.com>
2021-02-27 11:26:51 +0100

    splashpage

--- mesh_app_social/lib/splashpage.dart
+++ mesh_app_social/lib/splashpage.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:mesh_app_social/auth/login.dart';
+import 'package:mesh_app_social/frame.dart';
 
 class Splashpage extends StatefulWidget {
   @override
@@ -6,6 +9,23 @@ class Splashpage extends StatefulWidget {
 }
 
 class _SplashpageState extends State<Splashpage> {
+
+  @override
+  void initState() {
+    super.initState();
+    _navigateToRightPage();
+  }
+
+  _navigateToRightPage() {
+    bool userLoggedIn = true;
+
+    if(userLoggedIn) {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => Frame()));
+    } else {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => Login()));
+    }
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Container();

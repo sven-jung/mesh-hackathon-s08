0c839549ad705e0bbcde67f55a9c71ce4d4464cb
Hawk1401 <schalke.koch@web.de>
2021-02-27 15:53:05 +0100

    Added sln

---
+++ mesh_backend_social/Mesh_s08_API/Mesh_s08_API/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mesh_s08_API.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}

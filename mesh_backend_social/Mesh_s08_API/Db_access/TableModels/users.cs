a6d6d3ed7df3adc699bd55c36322d0b8f1ae5ae7
SeraLunatic <tamara@zetsche.de>
2021-02-27 21:40:26 +0100

    added table model

---
+++ mesh_backend_social/Mesh_s08_API/Db_access/TableModels/users.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Db_access.TableModels
+{
+    public class users
+    {
+        public string id_users { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+    }
+
+    public enum type { 
+        investor, founder
+    }
+}

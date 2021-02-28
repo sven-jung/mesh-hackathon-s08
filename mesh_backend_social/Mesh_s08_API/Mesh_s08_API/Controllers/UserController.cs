d99cd427e206bb621fa0bb8f8ab67d8731fbab00
Hawk1401 <schalke.koch@web.de>
2021-02-28 01:01:37 +0100

    adden user end point

--- mesh_backend_social/Mesh_s08_API/Mesh_s08_API/Controllers/UserController.cs
+++ mesh_backend_social/Mesh_s08_API/Mesh_s08_API/Controllers/UserController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Mesh_s08_API.Controllers
@@ -10,27 +11,30 @@ namespace Mesh_s08_API.Controllers
     {
         [Route("api/user/edit/tags")]
         [HttpPut]
+        [Authorize]
         public async Task<IActionResult> EditTags(string[] tasks)
         {
-            return BadRequest();
+            return Ok();
 
             //update data base
         }
 
         [Route("api/user/edit/name")]
         [HttpPut]
+        [Authorize]
         public async Task<IActionResult> EditName(string name)
         {
-            return BadRequest();
+            return Ok();
 
             //update DB
         }
 
         [Route("api/user/edit/type")]
         [HttpPut]
+        [Authorize]
         public async Task<IActionResult> EditType(int type)
         {
-            return BadRequest();
+            return Ok();
 
             //update DB
         }

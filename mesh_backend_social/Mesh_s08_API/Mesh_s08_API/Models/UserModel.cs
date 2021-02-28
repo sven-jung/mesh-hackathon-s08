29b6f1e52ea814d6ddf58fea501c086cfd367826
Hawk1401 <schalke.koch@web.de>
2021-02-28 10:07:07 +0100

    fixed model

--- mesh_backend_social/Mesh_s08_API/Mesh_s08_API/Models/UserModel.cs
+++ mesh_backend_social/Mesh_s08_API/Mesh_s08_API/Models/UserModel.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Db_access.TableModels;
 
 namespace Mesh_s08_API.Models
 {
     public class UserModel
     {
+        public string name { get; set; }
+        public string description { get; set; }
+        public type type { get; set; }
+        public string[] Tags { get; set; }
+        public string img { get; set; }
     }
 }
